6e3230e09dc0fc8eceb625a42ef2b9cf41e95a60
Sachin <barpandasachin@gmail.com>
2018-07-07 19:47:50 +0530

    Just Basics of Operators and Variables.

---
+++ Practice Javascript/ScriptVar.js
@@ -0,0 +1,68 @@
+//Variables Basics
+
+var name = "John";
+console.log(name);
+
+//var lastName = "Saiendrea";
+//console.log(lastName);
+
+var age = 20;
+console.log("Age of the person :" + age);
+
+var fullAge = true;
+console.log(fullAge);
+
+//Variables Some intermediate Stuffs
+ var name="Sam";
+ var age = 20;
+
+ console.log(name +" is of age "+age);
+ console.log(age+age);//o/p=40
+
+ //Let's see what is undefined is:-
+
+ var job,isCop;
+ console.log(job);//o/p undefined.
+
+ job = "Farmer";
+ isCop = false;
+
+ //console.log(name +" "+lastName+" age "+ age +" is a "+job + ". Is he a cop ?"+isCop);
+
+  name="Sam";
+  age = "twenty";
+  job = "programmer";
+  isCop = true;
+  //console.log(name +" "+lastName+" age "+ age +" is a "+job + ". Is he a cop ?"+isCop);
+
+//const lastName="Kali";
+
+console.log(name +" "+lastName+" age "+ age +" is a "+job + ". Is he a cop ?"+isCop);
+
+//USING Prompt
+
+var lastName = prompt("Type the last Name of Sam");
+console.log(lastName);
+
+//USING ALERT
+
+alert(name +" "+lastName+" age "+ age +" is a "+job + ". Is he a cop ?"+isCop);
+
+
+//USING Operators
+var now = 2016;
+var birthOfSam = 2016-20;
+
+BithyearOfSam = now - 20 * 2 ;
+
+console.log(BithyearOfSam);
+
+var ageOfMArk=50;
+var ageOfHawk=49;
+
+var ageofTim=(-48+23*12)+54; //first brackets then multiplication then division and then Subtraction
+
+console.log(ageofTim);
+var yearofsteezy = 1994;
+realageofsteezy = 21; 
+console.log(2018-5+10-realageofsteezy);
